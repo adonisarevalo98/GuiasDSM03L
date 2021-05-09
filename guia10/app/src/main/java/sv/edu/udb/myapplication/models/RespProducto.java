f1d13b8a3214261885deb79cecdc5a0de95af931
adonisarevalo98 <adonisarevalo503@gmail.com>
2021-05-08 18:14:08 -0600

    subida de guias

---
+++ guia10/app/src/main/java/sv/edu/udb/myapplication/models/RespProducto.java
@@ -0,0 +1,14 @@
+package sv.edu.udb.myapplication.models;
+
+public class RespProducto {
+    private boolean ok;
+    private Producto resultado;
+
+    public boolean getOk() {
+        return ok;
+    }
+
+    public Producto getResultado() {
+        return resultado;
+    }
+}
---
+++ guia10/app/src/main/java/sv/edu/udb/myapplication/models/Respuesta.java
@@ -0,0 +1,14 @@
+package sv.edu.udb.myapplication.models;
+
+public class Respuesta {
+    private boolean ok;
+    private int resultado;
+
+    public boolean getOk() {
+        return ok;
+    }
+
+    public int getResultado() {
+        return resultado;
+    }
+}
